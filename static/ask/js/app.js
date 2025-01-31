0b566cec40ce4deda22e5a24139455ebbcaed7e3
Mikhail Markov <mikhaiil@mmarkovv.ru>
2025-01-31 11:17:07 +0300

    added jquery to site

--- static/ask/js/app.js
+++
@@ -1,4 +0,0 @@
-"use strict"
-
-const questions = document.getElementsByClassName('question-article');
-console.log(questions);
---
+++ static/ask/js/jquery_test.js
@@ -0,0 +1,4 @@
+"use strict"
+
+$( "button.like-btn" ).html( "JQuery hello!");
+console.log("hello, world!");
